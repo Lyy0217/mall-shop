2aeaae627bed15e8ef31723b97da87a31d77aafb
liuyangyang03 <liuyangyang03@kuaishou.com>
2020-08-07 01:09:40 +0800

    创建用户

---
+++ mall-dev-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
@@ -0,0 +1,35 @@
+package com.imooc.pojo.bo;
+
+/**
+ * @author Lyy <liuyangyang03@kuaishou.com>
+ * Created on 2020-08-06
+ */
+public class UserBO {
+    private String username;
+    private String password;
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
--- mall-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ mall-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -1,19 +1,34 @@
 package com.imooc.service.impl;
 
+import java.util.Date;
+
+import com.imooc.enums.Sex;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
+import com.imooc.utils.DateUtil;
+import com.imooc.utils.MD5Utils;
+
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
 import tk.mybatis.mapper.entity.Example;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final String USER_FACE =
+            "http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_8_qAIlFXAAAcIhVPdSg994.png";
+
     @Autowired
     private UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -27,5 +42,37 @@ public class UserServiceImpl implements UserService {
         Users user = usersMapper.selectOneByExample(userExample);
 
         return user != null;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users createUser(UserBO userBO) {
+
+        String userId = sid.nextShort();
+
+        Users user = new Users();
+        user.setId(userId);
+        user.setUsername(userBO.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (Exception e) {
+            //todo
+            e.printStackTrace();
+        }
+        // 默认昵称同用户名
+        user.setNickname(userBO.getUsername());
+        // 默认用户头像
+        user.setFace(USER_FACE);
+        // 默认生日
+        user.setBirthday(DateUtil.stringToDate("1970-01-01"));
+        // 默认性别为 保密
+        user.setSex(Sex.secret.getType());
+
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
+
+        usersMapper.insert(user);
+
+        return user;
     }
 }
