de509c5ea9205d683c3ea82dda02be25efde1386
liuyangyang03 <liuyangyang03@kuaishou.com>
2020-08-20 00:27:36 +0800

    session

--- mall-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ mall-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.imooc.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,15 @@ public class HelloController {
     @GetMapping("/hello")
     public Object hello() {
         return "hello word~";
+    }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+        //        session.removeAttribute("userInfo");
+        return "ok";
     }
 }
