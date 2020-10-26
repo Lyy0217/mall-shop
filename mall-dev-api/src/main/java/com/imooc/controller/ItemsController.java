39a2527e06c61b8901e5e9fe42ffce603aadc911
liuyangyang03 <liuyangyang03@kuaishou.com>
2020-10-26 10:25:45 +0800

    a

---
+++ mall-dev-api/src/main/java/com/imooc/controller/ItemsController.java
@@ -0,0 +1,64 @@
+package com.imooc.controller;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imooc.enums.YseOrNo;
+import com.imooc.pojo.Carousel;
+import com.imooc.pojo.Category;
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+import com.imooc.pojo.bo.CarouselBO;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.ItemInfoVO;
+import com.imooc.pojo.vo.NewItemsVO;
+import com.imooc.service.CarouselService;
+import com.imooc.service.CategoryService;
+import com.imooc.service.ItemService;
+import com.imooc.utils.IMOOCJSONResult;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(value = "商品", tags = {"商品相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult subCat(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        Items item = itemService.queryItemById(itemId);
+        List<ItemsImg> itemsImgList = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecList = itemService.queryItemSpecList(itemId);
+        ItemsParam itemParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(item);
+        itemInfoVO.setItemImgList(itemsImgList);
+        itemInfoVO.setItemSpecList(itemsSpecList);
+        itemInfoVO.setItemParam(itemParam);
+
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+}
---
+++ mall-dev-pojo/src/main/java/com/imooc/pojo/vo/ItemInfoVO.java
@@ -0,0 +1,53 @@
+package com.imooc.pojo.vo;
+
+import java.util.List;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+/**
+ * 商品详情VO
+ *
+ * @author Lyy
+ * Created on 2020-10-14
+ */
+public class ItemInfoVO {
+    private Items item;
+    private List<ItemsImg> itemImgList;
+    private List<ItemsSpec> itemSpecList;
+    private ItemsParam itemParam;
+
+    public Items getItem() {
+        return item;
+    }
+
+    public void setItem(Items item) {
+        this.item = item;
+    }
+
+    public List<ItemsImg> getItemImgList() {
+        return itemImgList;
+    }
+
+    public void setItemImgList(List<ItemsImg> itemImgList) {
+        this.itemImgList = itemImgList;
+    }
+
+    public List<ItemsSpec> getItemSpecList() {
+        return itemSpecList;
+    }
+
+    public void setItemSpecList(List<ItemsSpec> itemSpecList) {
+        this.itemSpecList = itemSpecList;
+    }
+
+    public ItemsParam getItemParam() {
+        return itemParam;
+    }
+
+    public void setItemParam(ItemsParam itemParam) {
+        this.itemParam = itemParam;
+    }
+}
