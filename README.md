38ad38ea84a65319c108ce61701d587de2151f93
liuyangyang03 <liuyangyang03@kuaishou.com>
2021-11-01 23:54:20 +0800

    a

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 - 商城页面链接开发环境：http://localhost:8080/foodie-shop/
 
+- 用户中心链接：http://localhost:8080/foodie-center/
+
 - 商城页面链接生产环境：http://shop.t.mukewang.com/index.html
 
 - doc文档链接：http://localhost:8088/doc.html
---
+++ mall-dev-service/src/main/java/com/imooc/service/center/CenterUserService.java
@@ -0,0 +1,22 @@
+package com.imooc.service.center;
+
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.center.CenterUserBO;
+
+public interface CenterUserService {
+
+    /**
+     * 根据用户id查询用户信息
+     */
+    public Users queryUserInfo(String userId);
+
+    /**
+     * 修改用户信息
+     */
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO);
+
+    /**
+     * 用户头像更新
+     */
+    public Users updateUserFace(String userId, String faceUrl);
+}
