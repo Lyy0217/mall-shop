0da28eb35b271da2fdeda822ed07dff07351b84b
liuyangyang03 <liuyangyang03@kuaishou.com>
2021-11-15 10:16:14 +0800

    a

--- mall-dev-api/src/main/java/com/imooc/config/CorsConfig.java
+++ mall-dev-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -17,10 +17,10 @@ public class CorsConfig {
         // 1. 添加cors配置信息
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("http://shop.z.mukewang.com:8080");
-        config.addAllowedOrigin("http://center.z.mukewang.com:8080");
-        config.addAllowedOrigin("http://shop.z.mukewang.com");
-        config.addAllowedOrigin("http://center.z.mukewang.com");
+        config.addAllowedOrigin("http://lyyy.xyz:8080");
+        config.addAllowedOrigin("http://lyyy.xyz:8080");
+        config.addAllowedOrigin("http://lyyy.xyz");
+        config.addAllowedOrigin("http://lyyy.xyz");
         config.addAllowedOrigin("*");
 
         // 设置是否发送cookie信息
